8bdd9bd2736a05efb2ae2aec49d50b0c2e183dab
senique-dev <minirose42@gmail.com>
2024-08-14 22:38:18 +0900

    개발환경구성

---
+++ JBolgWeb/src/main/java/com/ssamz/jblog/JBolgWebApplication.java
@@ -0,0 +1,13 @@
+package com.ssamz.jblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBolgWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBolgWebApplication.class, args);
+	}
+
+}
